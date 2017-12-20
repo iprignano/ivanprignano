be16333175e7bfd6f3bd860aaff2148a3cc6e42a
Ivan <ivanprignano@gmail.com>
2017-12-20 11:47:40 +0100

    Fonts, shape animations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const main = () => {
   const shapes = document.querySelectorAll('.shape');
+  const shapeWrappers = document.querySelectorAll('.shapes-wrapper');
 
-  setTimeout(() => Array.from(shapes).forEach(shapeEl => shapeEl.classList.add('in-place')), 0);
+  setTimeout(() => {
+    Array.from(shapes).forEach(shapeEl => shapeEl.classList.add('in-place'));
+    Array.from(shapeWrappers).forEach(shapeWrapEl => shapeWrapEl.classList.add('in-place'));
+  }, 0);
 }
 
 document.onload = main();
